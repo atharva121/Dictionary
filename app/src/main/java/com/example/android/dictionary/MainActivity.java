80dabc4bbe4d3bd287e43e376556068843b53fd4
atharva121 <atharvasaraf@gmail.com>
2019-01-30 01:01:41 +0530

    Second commit

--- app/src/main/java/com/example/android/dictionary/MainActivity.java
+++ app/src/main/java/com/example/android/dictionary/MainActivity.java
@@ -2,15 +2,29 @@ package com.example.android.dictionary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        searchView = findViewById(R.id.search_view);
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchView.setIconified(false);
+            }
+        });
     }
 
     @Override
