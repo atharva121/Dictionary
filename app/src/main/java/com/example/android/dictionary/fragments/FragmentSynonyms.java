afaea8a52240e75e388a3fa3ea8e041822a5fc81
atharva121 <atharvasaraf@gmail.com>
2019-02-02 00:26:47 +0530

    Fourth commit

---
+++ app/src/main/java/com/example/android/dictionary/fragments/FragmentSynonyms.java
@@ -0,0 +1,24 @@
+package com.example.android.dictionary.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.dictionary.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentSynonyms extends Fragment {
+    public FragmentSynonyms() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_definition, container, false);
+        return view;
+    }
+}
