453439096693265679183a753d599309a24955e2
atharva121 <atharvasaraf@gmail.com>
2019-02-04 20:58:25 +0530

    Thirteenth commit

---
+++ app/src/main/java/com/example/android/dictionary/History.java
@@ -0,0 +1,13 @@
+package com.example.android.dictionary;
+
+public class History {
+    private String en_word;
+
+    public History(String en_word) {
+        this.en_word = en_word;
+    }
+
+    public String get_en_word(){
+        return en_word;
+    }
+}
